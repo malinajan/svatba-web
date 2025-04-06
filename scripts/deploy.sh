104d12cb51e98361ba9f8fc9ed449fff7909c403
Jan Horacek <me@apophis.cz>
2025-04-06 09:47:27 +0200

    Add autodeploy script.

---
+++ scripts/deploy.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo -e "Content-type: text/html\n"
+
+OUTPUT+=$(git fetch origin 2>&1)$'\n\n'
+if [ $? -eq 0 ]; then
+	OUTPUT+=$(git reset --hard origin/master 2>&1)$'\n\n'
+fi
+
+if [ $? -eq 0 ]; then
+	OUTPUT+=$(cd ../.. && make build 2>&1)$'\n\n'
+fi
+
+echo "$OUTPUT" | mail "me@apophis.cz" -s "[svatba-web] Deploy status"
+
